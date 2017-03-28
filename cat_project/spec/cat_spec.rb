6f29cda1873d37c1b60918cee619d11c09318601
John Shaw <shaw_fire@yahoo.com>
2017-03-28 12:54:25 +1100

    Two more cat tests added.
    Two more cat tests added.

--- cat_project/spec/cat_spec.rb
+++ cat_project/spec/cat_spec.rb
@@ -2,6 +2,7 @@ require 'cat'
 
 describe 'Cat' do
 
+  # '.' precedes the tesing of a class method (aka self.bowl_names_for_cats)
   describe '.bowl_names_for_cats' do # context is an alias for describe
 
     before do
@@ -18,6 +19,31 @@ describe 'Cat' do
       expected_bowl_names = ['PURRY', 'SCRATCHY', 'FERAL', 'TINY', 'LEO']
       expect(Cat.bowl_names_for_cats(@cats)).to match_array(expected_bowl_names)
     end
+  end
+
+  describe 'attributes' do
+    it "allows reading of :color" do
+      cat = Cat.new('Max', :black, 3)
+      expect(cat.color).to eq(:black)
+    end
+  end
+
+  # '#' precedes an instance method
+  describe '#name' do
+    it "allows reading of :name" do
+      cat = Cat.new('Max', :black, 3)
+      expect(cat.name).to eq('Max')
+    end
+  end
+
+
+  describe '.cats_with_color(cats, color)' do
+  end
+
+  describe '.colors_for_cats(cats)' do
+  end
+
+  describe '.oldest_cat_in(cats)' do
   end
 
 end
