940d008cf5e18aae35450265f9fbc1f72696ecd2
John Shaw <shaw_fire@yahoo.com>
2017-03-29 12:07:02 +1100

    Added failing test.

--- codewars_spoiler_alert/spec/encrypt_spec.rb
+++ codewars_spoiler_alert/spec/encrypt_spec.rb
@@ -6,6 +6,10 @@ describe 'Encrypt' do
     it "returns string encrypted 0 times" do
       expect(encrypt("This is a test!",0)).to eq("This is a test!")
     end
+
+    it "returns string encrypted 1 time" do
+      expect(encrypt("This is a test!",1)).to eq("hsi  etTi sats!")
+    end
   end
 
 end
